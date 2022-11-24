b0b34862d06046135cedac44467987fe27657d35
Daniel Ferreira <64641858+danielfninja775@users.noreply.github.com>
2022-11-24 12:56:26 +0100

    Add files via upload

---
+++ build/static/trjkqvDSl0Q9S1Jmuo1Qe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d7eeaac4-84b002ada70579e7.js","static/css/099c329075d93869.css","static/chunks/pages/index-971865aa87fd6b0b.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
